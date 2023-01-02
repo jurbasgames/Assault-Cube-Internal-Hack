9bd848f8d47e850948fd35104f20ea383c4cc066
João Pedro Duarte <jpduarte@ejcm.com.br>
2023-01-02 14:34:06 -0300

    fixed win32 error

--- Assault Cube Internal Hack/dllmain.cpp
+++ Assault Cube Internal Hack/dllmain.cpp
@@ -1,62 +1,86 @@
-// dllmain.cpp : Define o ponto de entrada para o aplicativo DLL.
+// dllmain.cpp : Defines the entry point for the DLL application.
 #include "pch.h"
-#include <iostream>
-#include "mem.h"
-#include <windows.h>
 #include <stdio.h>
+#include <iostream>
+#include <vector>
+
+
+uintptr_t FindDMAAddy(uintptr_t ptr, std::vector<unsigned int> offsets) {
+    uintptr_t addr = ptr;
+    for (unsigned int i = 0; i < offsets.size(); ++i)
+    {
+        addr = *(uintptr_t*)addr;
+        addr += offsets[i];
+    }
+    return addr;
+
+}
 
 DWORD WINAPI HackThread(HMODULE hModule)
 {
+    //Create Console
     AllocConsole();
     FILE* f;
     freopen_s(&f, "CONOUT$", "w", stdout);
-    
-    std::cout << "F1 - Infinite Ammo\nHOME - Exit\n";
+
+    std::cout << "F1 - Infinite heath(Not working)\nF2 - Infinite ammo\nF3 - No recoil(Not working)\n";
 
     uintptr_t moduleBase = (uintptr_t)GetModuleHandle(L"ac_client.exe");
 
+    //calling it with NULL also gives you the address of the .exe module
+    moduleBase = (uintptr_t)GetModuleHandle(NULL);
 
-    bool bAmmo = false;
-    std::vector<unsigned int> ammoOffsets = { 0x368 ,0x14 ,0x0 };
-    
+    bool bHealth = false, bAmmo = false, bRecoil = false;
+
     while (true)
     {
-        if (GetAsyncKeyState(VK_F1) & 1) {
-            bAmmo = !bAmmo;
-
-        }
-        if (GetAsyncKeyState(VK_HOME) & 1) {
+        if (GetAsyncKeyState(VK_END) & 1)
+        {
             break;
         }
 
-        
-        if (bAmmo) {
-            uintptr_t ammoAddr = FindDMAAddy(moduleBase + 0x17E0A8, ammoOffsets);
-            int* ammo = (int*)ammoAddr;
-            *ammo = 999;
+        if (GetAsyncKeyState(VK_F2) & 1)
+            bHealth = !bHealth;
+        if (GetAsyncKeyState(VK_F1) & 1)
+        {
+            bAmmo = !bAmmo;
         }
-        
-        Sleep(5);
 
+        uintptr_t* localPlayerPtr = (uintptr_t*)(moduleBase + 0x17E0A8);
+
+
+        if (localPlayerPtr)
+        {
+            if (bHealth)
+            {
+                *(int*)(*localPlayerPtr + 0xF8) = 1337;
+            }
+
+            if (bAmmo)
+            {
+                *(int*)FindDMAAddy(moduleBase + 0x17E0A8, { 0x368, 0x14, 0x0 }) = 1337;
+            }
+
+        }
+        Sleep(5);
     }
 
     fclose(f);
     FreeConsole();
     FreeLibraryAndExitThread(hModule, 0);
     return 0;
-
 }
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved)
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+    DWORD  ul_reason_for_call,
+    LPVOID lpReserved
+)
 {
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
-        CloseHandle(
-            CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)HackThread, hModule, 0, nullptr)
-        );
+        CloseHandle(CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)HackThread, hModule, 0, nullptr));
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
     case DLL_PROCESS_DETACH:
@@ -64,4 +88,3 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     }
     return TRUE;
 }
-
